c732566eec65e0433e1be804914ecd4ff5562c53
Mark Kopenga <mkopenga@gmail.com>
2018-03-07 14:28:42 +0100

    Made the status report better

--- build/api/imports.php
+++ build/api/imports.php
@@ -1,10 +1,34 @@
 <?php
 include_once './env.php';
 
-header('Content-Type: application/json');
+$pdo = null;
+
+// set the header of all outputs to json because this is api data
+header('Content-type:application/json;charset=utf-8');
 
 // this array contains info about the server
-$report = array();
+$report = array(
+  'SQL' => True,
+  'DB' => True,
+  'ENV' => True
+);
+
+// return server status
+function returnStatus () {
+  if ($GLOBALS['report']['SQL'] == True) {
+    try {
+      $GLOBALS['pdo']->query("SELECT * FROM users LIMIT 1");
+      $GLOBALS['report']['Tabel and database'] = 'Tabel and database exsist';
+    } catch (Exception $e) {
+      $GLOBALS['report']['Tabel and database'] = 'Tabel and/or database are not exsisting, ERROR:' . $e->getMessage();
+      $GLOBALS['report']['DB'] = False;
+    }
+  } else {
+    $GLOBALS['report']['Tabel and database'] = 'Tabel and/or database are not exsisting';
+    $GLOBALS['report']['DB'] = False;
+  }
+  return $GLOBALS['report'];
+}
 
 // if the varialbes are not defined try to use a .env file
 $envFile = './.env';
@@ -18,14 +42,22 @@ if ($env['SQLusername'] == '' && $env['SQLpassword'] == '' && file_exists($envFi
   $report['env'] = 'env.php is not filled in, using .env file';
 } elseif ($env['SQLusername'] == '' && $env['SQLpassword'] == '') {
   $report['env'] = 'env.php is not filled in no .env file found as alternative';
+  $report['ENV'] = False;
 } else {
   $report['env'] = 'using env.php data';
 }
 
-try {
-  $conn = new PDO("mysql:host=" . $env["SQLserver"] . ";dbname=" . $env["SQLdatabaseName"], $env["SQLusername"], $env["SQLpassword"]);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $report['databaseConnection'] = 'connected to database';
-} catch(PDOException $e) {
-  $report['databaseConnection'] = "Connection failed: " . $e->getMessage();
+if ($report['ENV'] == True) {
+  try {
+    $pdo = new PDO("mysql:host=" . $env["SQLserver"] . ";dbname=" . $env["SQLdatabaseName"], $env["SQLusername"], $env["SQLpassword"]);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $GLOBALS['pdo'] = $pdo;
+    $GLOBALS['report']['Sql Connection'] = 'connected to database';
+  } catch(PDOException $e) {
+    $GLOBALS['report']['Sql Connection'] = "Connection failed, ERROR:" . $e->getMessage();
+    $GLOBALS['report']['SQL'] = False;
+  }
+} else {
+  $GLOBALS['report']['Sql Connection'] = "Can't connect to SQL without login data";
+  $GLOBALS['report']['SQL'] = False;
 }
--- build/api/status.php
+++ build/api/status.php
@@ -2,4 +2,4 @@
 include_once './imports.php';
 // this file reports errors
 
-echo json_encode($report);
+echo json_encode(returnStatus());
