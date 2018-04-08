ebdc305896aaf3b9183fd141af288f0254361fda
Mark Kopenga <mkopenga@gmail.com>
2018-04-08 21:31:45 +0200

    :sparkles: Started working on sub list

--- build/api/list.php
+++ build/api/list.php
@@ -2,64 +2,119 @@
 include_once './imports.php';
 // this file will handel the requests for lists
 
-if (isset($_GET['what'])) {
-  $_list = (int)$_GET['what'];
+if (
+  $_SERVER['REQUEST_METHOD'] === 'POST' 
+  && isset($_POST['listname']) 
+  && $_POST['listname'] != ''
+) {
+  $test = SQLfetch("
+    SELECT *
+    FROM list
+    WHERE name = :listName
+  ", array(
+    ':listName' => $_POST['listname']
+  ));
+  $test2 = SQLfetch("
+    SELECT *
+    FROM list
+    WHERE inList = :inList
+  ", array(
+    ':inList' => 'defualt'
+  ));
+  if ($test['status'] && !isset($test['data'][0]) && $test2['status'] && isset($test2['data'][0])) {
+    SQLfetch("
+      INSERT INTO list
+      (premission, name, inList, premissionToCreate)
+      VALUES (\"3\", :name, :inList, \"1\")
+    ", array(
+      ':name' => $_POST['listname'],
+      'inList' => (string)$test2['data'][0]['ID']
+    ));
+    echo json_encode(array(
+      'status' => True
+    ));
+  } else {
+    echo json_encode(array(
+      'status' => False
+    ));
+  }
 } else {
-  $_list = 0; // set list as null this will be the home page
-}
 
-$data = SQLfetch("
-  SELECT messages.*, users.username AS username 
-  FROM (messages
-  INNER JOIN users ON messages.userID = users.ID) 
-  WHERE inList = :list AND start = 'true'
-", array(
-  ':list' => $_list
-));
+  if (isset($_GET['what'])) {
+    $_list = (int)$_GET['what'];
+  } else {
+    $_list = 0; // set list as null this will be the home page
+  }
 
-$listinf = SQLfetch("
-  SELECT *
-  FROM list 
-  WHERE " . (($_list == 0) ? "name = 'home'" : "ID = :list"), 
-  array(
-  ':list' => $_list
-));
+  $data = SQLfetch("
+    SELECT messages.*, users.username AS username 
+    FROM (messages
+    INNER JOIN users ON messages.userID = users.ID) 
+    WHERE inList = :list AND start = 'true'
+  ", array(
+    ':list' => $_list
+  ));
 
-$listname = ($listinf['status'] == True && isset($listinf['data'][0])) ? $listinf['data'][0]['name'] : "---";
+  $listinf = SQLfetch("
+    SELECT *
+    FROM list 
+    WHERE " . (($_list == 0) ? "inList = 'defualt'" : "ID = :list
+  "), array(
+    ':list' => $_list
+  ));
 
-if ($data['status'] && isset($data['data'][0])) {
-  // add every item to a new array
-  $_data = array();
-  foreach ($data['data'] as $key => $value) {
-    array_push($_data, array(
-      'created' => $value['created'],
-      'premission' => $value['premission'],
-      'id' => $value['ID'],
-      'title' => $value['title'],
-      'username' => $value['username']
+  if ($_list == 0 && $listinf['status'] && isset($listinf['data'][0])) {
+    $sublists = SQLfetch("
+      SELECT *
+      FROM list
+      WHERE inList = :inList
+    ", array(
+      'inList' => $listinf['data'][0]['ID']
+    ));
+    if ($sublists['status'] && isset($sublists['data'][0])) {
+      $sublists = $sublists['data'];
+    } else {
+      $sublists = array();
+    }
+  } else {
+    $sublists = array();
+  }
+
+  $listname = ($listinf['status'] == True && isset($listinf['data'][0])) ? $listinf['data'][0]['name'] : "---";
+
+  if ($data['status'] && isset($data['data'][0])) {
+    // add every item to a new array
+    $_data = array();
+    foreach ($data['data'] as $key => $value) {
+      array_push($_data, array(
+        'created' => $value['created'],
+        'premission' => $value['premission'],
+        'id' => $value['ID'],
+        'title' => $value['title'],
+        'username' => $value['username']
+      ));
+    }
+
+    echo json_encode(array(
+      'title' => $listname,
+      'messages' => $_data,
+      'lists' => $sublists,
+      'messagesStatus' => isset($data['data'][0])
+    ));
+  } elseif ($data['status']) {
+    echo json_encode(array(
+      'title' => $listname,
+      'messages' => array(),
+      'lists' => $sublists,
+      'messagesStatus' => isset($data['data'][0])
+    ));
+  } else {
+    echo json_encode(array(
+      'status' => False,
+      'why' => 'this lists doesn\'t exsist or is empty',
+      'short' => 'EmptyOrAway',
+      'report' => $report
     ));
   }
 
-  $list = array();
-
-  echo json_encode(array(
-    'title' => $listname,
-    'messages' => $_data,
-    'lists' => $list,
-    'messagesStatus' => isset($data['data'][0])
-  ));
-} elseif ($data['status']) {
-  echo json_encode(array(
-    'title' => $listname,
-    'messages' => array(),
-    'lists' => array(),
-    'messagesStatus' => isset($data['data'][0])
-  ));
-} else {
-  echo json_encode(array(
-    'status' => False,
-    'why' => 'this lists doesn\'t exsist or is empty',
-    'short' => 'EmptyOrAway',
-    'report' => $report
-  ));
 }
--- dynamic/js/components/list.js
+++ dynamic/js/components/list.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Setup from '../components/setup.js'
 import {functions} from '../imports/functions.js'
+import MDinput from '../components/md-input.js'
+import MDdelete from 'react-icons/lib/md/delete'
 import {OpenMessage, CreateMessage} from '../components/message.js'
 import urlhandeler from '../imports/urlhandeler.js'
 
@@ -15,7 +17,10 @@ class List extends Component {
       forumName: '',
       LoginStatus: inputs.LoginStatus,
       showSetup: false,
-      list: inputs.list
+      list: inputs.list,
+      createList: false,
+      createListName: '',
+      createListWorking: false
     }
     this.fetchList(inputs.list || 0)
     this.urlHandeler = new urlhandeler({
@@ -69,17 +74,42 @@ class List extends Component {
         <div className="title">
           <h2>{this.state.forumName}</h2>
         </div>
-        {(this.state.LoginStatus.logedin) ? <div className="buttons">
-          <button
-            onClick={() => {
-              CreateMessage()
-              this.urlHandeler.changePath('/message.php')
-            }}
-          >Nieuw bericht</button>
-        </div> : ''}
-        <div className="lists list">
-          {(this.state.subForums[0]) ? <h3>Sub forums</h3> : ''}
-        </div>
+        {(this.state.LoginStatus.logedin) ? 
+          <div className="buttons">
+            <button
+              onClick={() => {
+                CreateMessage()
+                this.urlHandeler.changePath('/message.php')
+              }}
+            >Nieuw bericht</button>
+            { (Number(this.state.LoginStatus.userData.premission) == 2 || Number(this.state.LoginStatus.userData.premission) == 3) ?
+              <button
+                onClick={() => {
+                  this.setState({
+                    createList: true
+                  })
+                }}
+              >Nieuwe lijst</button>
+            : '' }  
+          </div> 
+        : ''}
+        {(this.state.subForums[0]) ? 
+          <div className="lists list">
+            <h3>Sub lijsten</h3>
+            {this.state.subForums.map((el, id) => 
+              <div key={id} className="listitem">
+                <div className="itemTitle" onClick={() => {
+                  log(el)
+                }}>{el.name}</div>
+                { (Number(this.state.LoginStatus.userData.premission) == 3) ?
+                  <div className="options">
+                    <MDdelete className="remove icon" size={25}/>
+                  </div>
+                : '' }
+              </div>
+            )}
+          </div>
+        : ''}
         <div className="messages list">
           {(this.state.messages[0]) ? <h3>Messages</h3> : ''}
           {this.state.messages.reverse().map((msg, id) => 
@@ -95,6 +125,60 @@ class List extends Component {
             </div>
           )}
         </div>
+        { (this.state.createList) ? 
+          <div className="newList">
+            <div className="newListInside">
+              <h2>Creëer een nieuwe lijst</h2>
+              <div className="inputs">
+                <MDinput
+                  label="Lijst naam"
+                  onChange={data => this.setState({
+                    createListName: data
+                  })}
+                />
+              </div>
+              <div className="buttons">
+                <button
+                  disabled={this.state.createListWorking}
+                  onClick={() => {
+                    this.setState({
+                      createList: false
+                    })
+                  }}
+                >Terug</button>
+                <button
+                  disabled={!this.state.createListName || this.state.createListWorking}
+                  onClick={() => {
+                    this.setState({
+                      createListWorking: true
+                    }, () => {
+                      functions.fetch('./api/list.php', 'json', (data) => {
+                        if (data.status) {
+                          this.fetchList(this.state.list)
+                          this.setState({
+                            createList: false,
+                            createListWorking: false,
+                            createListName: ''
+                          })
+                        } else {
+                          this.setState({
+                            createListWorking: false
+                          })
+                        }
+                      }, {
+                        cache: 'no-cache',
+                        method: 'POST',
+                        body: {
+                          listname: this.state.createListName
+                        }
+                      })
+                    })
+                  }}
+                >Lijst aan maken</button>
+              </div>
+            </div>
+          </div>
+        : ''}
         <Setup 
           show={this.state.showSetup}
           callback={(show) => {
