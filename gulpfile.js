c1a2fa69dffca4c12a4ca5aff737452c88774404
Mark Kopenga <mkopenga@gmail.com>
2018-04-13 10:52:48 +0200

    :bookmark: Worked on gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,24 @@ const exec = util.promisify(require('child_process').exec)
 const log = console.log
 let taker = new Undertaker()
 
+let start = name => {
+  process.stdout.write(' ⌛' + colors.yellow(name) + '\r')
+}
+
 let report = (name, err, cb) => {
   if (err) {
-    log(colors.red('got error with: ') + colors.red.bold(name))
+    log(colors.red('\ngot error with: ') + colors.red.bold(name))
     console.error(err)
     process.exit()
   } else {
-    log(colors.green('\u2713 ' + name))
+    process.stdout.write(' ✓ ' + colors.green(name) + ' \r \n')
     cb()
   }
 }
 
 let clearBuildFolder = 'clear build folder'
 taker.task(clearBuildFolder, cb => {
+  start(clearBuildFolder)
   try {
     let toRemove = [
       'index.php',
@@ -37,37 +42,39 @@ taker.task(clearBuildFolder, cb => {
 })
 
 let createBuild = 'run webpack to create a clean build'
-taker.task(createBuild, cb => {
-  async function run () {
-    const { stdout, stderr } = await exec('webpack --display=minimal --config \"webpack.config.js\"')
-    report(createBuild, (stdout.indexOf('DUNE') != -1) ? ('ERROR: ' + stderr) : false, cb)
-  }
-  run()
+taker.task(createBuild, async cb => {
+  start(createBuild)
+  const { stdout, stderr } = await exec('webpack --display=minimal --config \"webpack.config.js\"')
+  report(createBuild, (stdout.indexOf('DUNE') != -1) ? ('ERROR: ' + stderr) : false, cb)
 })
 
 let createFolder = 'make sure release folder exsists'
-taker.task(createFolder, cb => 
+taker.task(createFolder, cb => {
+  start(createFolder)
   fs.ensureDir('./release/', err => 
     report(createFolder, err, cb)
   )
-)
+})
 
 let copy = 'copy files'
-taker.task(copy, cb => 
+taker.task(copy, cb => {
+  start(copy)
   fs.copy('./build/', './release/', err => 
     report(copy, err, cb)
   )
-)
+})
 
 let rmJunk = 'removing junk and all kinds of other files'
-taker.task(rmJunk, cb => 
+taker.task(rmJunk, cb => {
+  start(rmJunk)
   fs.remove('./release/api/.env', err => 
     report(rmJunk, err, cb)
   )
-)
+})
 
 let createEnv = 'creating original env.php file'
-taker.task(createEnv, cb =>
+taker.task(createEnv, cb => {
+  start(createEnv)
   fs.outputFile('./release/api/env.php', `
     <?php
     $env = array(
@@ -76,9 +83,51 @@ taker.task(createEnv, cb =>
   `, err => 
     report(createEnv, err, cb)
   )
-)
+})
 
-// Todo: Add: https://github.com/archiverjs/node-archiver
+let createZip = 'creating zip from folder content'
+taker.task(createZip, cb => {
+  start(createZip)
+  fs.removeSync(__dirname + '/release.zip')
+  let output = fs.createWriteStream(__dirname + '/release.zip')
+  let archive = archiver('zip', {zlib: { level: 9 }})
+  output.on('close', () => {
+    report(createZip, false, cb)
+  })
+  output.on('end', () => {
+    report(createZip, true, cb)
+  })
+  archive.on('warning', err => {
+    report(createZip, err, cb)
+  })
+  archive.on('error', err => {
+    report(createZip, err, cb)
+  })
+  archive.pipe(output)
+  archive.file('release/index.php', { name: 'index.php' })
+  archive.file('release/message.php', { name: 'message.php' })
+  archive.file('release/settings.php', { name: 'settings.php' })
+  archive.directory('release/api/', 'api')
+  archive.directory('release/icons/', 'icons')
+  archive.directory('release/js/', 'js')
+  archive.finalize()
+})
+
+let cleanUp = 'cleanup release files'
+taker.task(cleanUp, cb => {
+  start(cleanUp)
+  fs.remove('./release', err => 
+    report(cleanUp, err, cb)
+  )
+})
+
+let dune = 'dune'
+taker.task(dune, cb => {
+  log('\n')
+  log('Sucsessfull created ' + 'release.zip'.bold.green + ' file ')
+  log('\n')
+  cb()
+})
 
 gulp.task('default', taker.series(
   clearBuildFolder,
@@ -86,5 +135,8 @@ gulp.task('default', taker.series(
   createFolder, 
   copy,
   rmJunk,
-  createEnv
+  createEnv,
+  createZip,
+  cleanUp,
+  dune
 ))
